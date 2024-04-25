9cf026586fe12016688724768d5755ab3b6ac2e6
Harsh <hs5686584@gmail.com>
2024-04-26 00:45:15 +0530

    worked with useState

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -3,10 +3,14 @@ import { CORE_CONCEPTS } from "./data.js";
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
+import { useState } from "react";
+import { EXAMPLES } from "./dataExamples.js";
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState("components");
   function eventhandler(params) {
-    console.log("clicked button is ->", params);
+    setSelectedTopic(params);
+    console.log(selectedTopic);
   }
   return (
     <div>
@@ -42,33 +46,40 @@ function App() {
           <menu>
             <TabButton
               method={() => {
-                eventhandler("Components");
+                eventhandler("components");
               }}
             >
               Components
             </TabButton>
             <TabButton
               method={() => {
-                eventhandler("JSX");
+                eventhandler("jsx");
               }}
             >
               JSX
             </TabButton>
             <TabButton
               method={() => {
-                eventhandler("Props");
+                eventhandler("props");
               }}
             >
               Props
             </TabButton>
             <TabButton
               method={() => {
-                eventhandler("State");
+                eventhandler("state");
               }}
             >
               State
             </TabButton>
           </menu>
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
