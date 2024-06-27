d466f9d3bab5aa3f4907b443b50937d8fc7713a6
Harsh <hs5686584@gmail.com>
2024-06-28 01:02:42 +0530

    completed 2 way binding with input field

--- tic-tac-toe-project/src/App.jsx
+++ tic-tac-toe-project/src/App.jsx
@@ -1,5 +1,17 @@
+import Player from "./components/Player.jsx";
+
 function App() {
-  return <p>coming soon</p>;
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player initialName={"player 1"} playerSymbol={"x"} />
+          <Player initialName={"player 2"} playerSymbol={"o"} />
+        </ol>
+      </div>
+      Log
+    </main>
+  );
 }
 
 export default App;
---
+++ tic-tac-toe-project/src/components/Player.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+export default function Player({ initialName, playerSymbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
+
+  const editTriger = () => {
+    setIsEditing((editing) => !editing);
+  };
+
+  function handleChange(e) {
+    setPlayerName(e.target.value);
+  }
+
+  let playerHtml = <span className="player-name">{playerName}</span>;
+  let buttonText = <button onClick={editTriger}>Edit</button>;
+
+  if (isEditing == true) {
+    playerHtml = (
+      <input type="text" value={playerName} onChange={handleChange}></input>
+    );
+    buttonText = <button onClick={editTriger}>Save</button>;
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {playerHtml}
+        <span className="player-symbol">{playerSymbol}</span>
+      </span>
+      {buttonText}
+    </li>
+  );
+}
