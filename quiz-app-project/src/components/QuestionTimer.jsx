91680af9aa9477e824302ab9f0ff4f50c4e0587e
Harsh <hs5686584@gmail.com>
2024-08-16 23:46:56 +0530

    updated quiz app

--- quiz-app-project/src/components/QuestionTimer.jsx
+++ quiz-app-project/src/components/QuestionTimer.jsx
@@ -3,15 +3,15 @@ import React, { useState, useEffect } from "react";
 export default function QuestionTimer({ timeout, onTimeout }) {
   const [remainingTime, setRemainingTime] = useState(timeout);
 
-  //   useEffect(() => {
-  //     setTimeout(onTimeout, timeout);
-  //   }, [onTimeout, timeout]);
+  useEffect(() => {
+    setTimeout(onTimeout, timeout);
+  }, [onTimeout, timeout]);
 
-  //   useEffect(() => {
-  //     setInterval(() => {
-  //       setRemainingTime((prevRemainTime) => prevRemainTime - 100);
-  //     }, 100);
-  //   }, []);
+  useEffect(() => {
+    setInterval(() => {
+      setRemainingTime((prevRemainTime) => prevRemainTime - 100);
+    }, 100);
+  }, []);
 
   return <progress id="question-time" max={timeout} value={remainingTime} />;
 }
