bc4f8d130e7e085c8a03da989edd4f92e35a9f0f
Harsh <hs5686584@gmail.com>
2024-08-21 23:47:59 +0530

    use reducer hook learning

---
+++ hook-useReducer/src/App.jsx
@@ -0,0 +1,34 @@
+import { useReducer, useState } from "react";
+
+const initialState = 5;
+
+const reducer = (state, action) => {
+  if (action.type === "increment") {
+    return state + 1;
+  }
+  if (action.type === "decrement") {
+    return state - 1;
+  }
+};
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <>
+      <div>
+        <h1>{state}</h1>
+        <button onClick={() => dispatch({ type: "increment" })}>
+          Increase
+        </button>
+        <button onClick={() => dispatch({ type: "decrement" })}>
+          decrease
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default App;
